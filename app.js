78b9517b59d0dfe5b66de49dc490232038f2ad19
chandan-kj <61055870+chandan-kj@users.noreply.github.com>
2020-12-04 14:30:52 +0530

    docs: added api

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector('#txt-input');
 var outputDiv = document.querySelector('#output')
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+  console.log("error occured",error)
+  alert("Site is down try again after some times")
+}
+
 function clickHandler() {
-  outputDiv.innerHTML = "hygdigdied " + textInput.value
+  var inputText = textInput.value;// input
+
+  //calling server
+  fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerHTML = translatedText// output
+  })
+  .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickHandler);
